590e3d86898ced479827d84b36a9ddd58950b1b5
Malek Boufahja <boufahja@weringroup.com>
2023-03-20 15:40:30 +0100

    selenium dockerfile v1 ok

--- dockerfiles/selenium-docker/test-chrome.py
+++ dockerfiles/selenium-docker/test-chrome.py
@@ -7,7 +7,7 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
  
 driver.get("https://python.org")
 print(driver.title)
--- dockerfiles/selenium-docker/test-firefox.py
+++ dockerfiles/selenium-docker/test-firefox.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
  
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
  
 driver.get("https://python.org")
 print(driver.title)
